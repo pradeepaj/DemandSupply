a24b60048953efc52e835502fb8cdcf5c1dfebfe
User1 <anuradha.krishnanov7@gmail.com>
2019-08-10 11:30:10 +0530

    Project-struture

---
+++ src/main/java/com/hcl/demand/supply/controller/DemandSupplyController.java
@@ -0,0 +1,13 @@
+package com.hcl.demand.supply.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demandsupply")
+public class DemandSupplyController {
+	
+	@GetMapping("/showDemand")
+
+}
